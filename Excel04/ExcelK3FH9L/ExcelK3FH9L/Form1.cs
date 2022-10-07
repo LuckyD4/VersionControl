a3f753d3b29e6e1a8769762be4cb8d1458aff053
Dániel Szerencsés <daniel.szerencses@stud.uni-corvinus.hu>
2022-10-07 11:13:23 +0200

    fourth

--- Excel04/ExcelK3FH9L/ExcelK3FH9L/Form1.cs
+++ Excel04/ExcelK3FH9L/ExcelK3FH9L/Form1.cs
@@ -14,12 +14,16 @@ namespace ExcelK3FH9L
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
 
         private void LoadData()
@@ -29,6 +33,29 @@ namespace ExcelK3FH9L
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void CreateExcel()
+        {
+           
+        }    
+        private void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+            };
         }
     }
 }
